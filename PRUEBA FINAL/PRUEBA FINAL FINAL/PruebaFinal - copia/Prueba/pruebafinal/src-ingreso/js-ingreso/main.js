4ab6b7f2fe5e03b9998f371cfd2b3585c1833317
2Goddie7 <diego.mullo@epn.edu.ec>
2024-11-16 15:10:41 -0500

    Add files via upload

---
+++ PRUEBA FINAL/PRUEBA FINAL FINAL/PruebaFinal - copia/Prueba/pruebafinal/src-ingreso/js-ingreso/main.js
@@ -0,0 +1,60 @@
+// ------------------------- BEGGINER ---------------------
+//internet
+//TCP-IP
+//ISP ME DA IP Y UN MODEN/ROUTER
+//WEB (UTILIZA PAGINAS WEB + NAVEGADOR WEB)
+//CLIENTE - SERVIDOR(ARQUIETECTURA WEB)
+//HTTP CAMINO INSEGURO HTTPS SEGURO
+//DOMINIO Y SUBDOMINIO
+//HOSTIN Y DOMINIO
+
+// ------------- BASIC -----------------------
+//Roles en el Desarrollo web
+//backend crear apis
+//front end crear interfaces de usuario
+// full stack All in one
+
+//interfaz
+//interfaz de usuario (UI)
+//experiencia de usuario (UX)
+//1. header - bodyu - footer
+//2.menos es mas
+//3.adaptable()
+//4.URL AMIGABLE
+
+// ------------------ INTERMEDIATE --------------
+
+//Fases de desarrollo de aplicaciones frontend (aplicaciones del lado del cliente)
+//Requisitos (Artefactos)(RR-HU-PB-SB)
+//Wireframe
+//Mockup
+//Prototipo
+
+// -------------- ADVANCE ---------------
+//HTML
+//Etiquetas semanticas(body-nav-p-h1-aside-footer-main-ul-ol-figure)
+// <a href="#">Nosotros</a>(Apertura y cierre)
+//Atributos y valores href="#" - loading ="lazy"
+//Recursos extras
+
+//CSS lenguaje que da estilos a HTML
+//Reglas (selector-Propiedad-valor)
+/*
+
+
+ */
+//Diseño responsive
+// 1.- Media Queries (breakpoints)
+// 2.- Flexbox
+// 3.- Grid
+
+//Mobile first and desktop first
+//Deploy
+//Sandbox - ambiente controlado
+//Testing
+// 1.- Unitarias
+// 2.- Compatibilidad
+// 3.- Carga
+// 3.- Rendimiento
+// 3.- Aceptación
+
